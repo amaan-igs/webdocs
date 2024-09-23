8b4a8d62ed11276de070a5c2620dc202d6a9badf
IGS <amaanulhaq55@gmail.com>
2024-09-24 02:08:43 +0500

    CNAME

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/webdocs/',
+  base: '/amaan.inotech-pk.com/',
   title: "Amaan",
   description: "Project Documentation",
   themeConfig: {
