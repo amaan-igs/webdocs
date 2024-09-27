60e02e73603ca9adc1da95ab11ebd19b2a9b3ef6
IGS <amaanulhaq55@gmail.com>
2024-09-28 00:11:16 +0500

    ico +

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,20 +1,30 @@
+// .vitepress/config.js
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
   base: '/',
   sitemap: {
-    hostname: 'http://dev.inotech-pk.com', // my domain
+    hostname: 'https://dev.inotech-pk.com', // Updated to HTTPS
     lastmodDateOnly: false,
   },
   title: "Amaan",
   description: "Project Documentation",
+  
+  // Add the head property here
+  head: [
+    ['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    // Optional: Add Apple Touch Icon for better compatibility
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    // Optional: Add a manifest file for progressive web apps
+    ['link', { rel: 'manifest', href: '/site.webmanifest' }]
+  ],
+  
   themeConfig: {
-
+    logo: '/LOGO-NOTEXT-BG-N.png',  // Use a separate logo file
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Summary', link: '/intro' }
     ],
-
     sidebar: [
       {
         text: 'Introduction',
@@ -35,9 +45,7 @@ export default defineConfig({
           { text: 'Contact', link: '/contact' }, 
         ]
       }
-
     ],
-
     socialLinks: [
       { icon: 'github', link: 'https://github.com/amaan-igs' },
       { icon: 'linkedin', link: 'https://www.linkedin.com/in/amaanulhaqsiddiqui/' }
