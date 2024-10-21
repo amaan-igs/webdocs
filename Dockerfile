d7d083765778d568e1883864399ed051324ca97e
Amaan Ul Haq Siddiqui <amaanulhaq55@gmail.com>
2024-10-21 20:29:54 +0500

    containerized

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:lts-alpine
+
+ENV NODE_ENV=development
+
+WORKDIR /usr/src/app
+
+COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
+
+RUN npm install --silent && mv node_modules ../
+
+COPY . .
+
+EXPOSE 5173
+
+RUN chown -R node /usr/src/app
+
+USER node
+
+CMD ["npm", "run", "docs:dev", "--", "--host"]
--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -9,33 +9,33 @@ import {
 } from "./chunk-ONFHZ5OS.js";
 
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "/usr/src/app/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
 // node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
-import "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import { default as default3 } from "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
-import { default as default4 } from "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
-import { default as default5 } from "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
-import { default as default6 } from "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default7 } from "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default8 } from "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default9 } from "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default10 } from "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
-import { default as default11 } from "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default12 } from "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default13 } from "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default14 } from "C:/Users/LENOVO/Desktop/Code/Projects/webdocs/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "/usr/src/app/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "/usr/src/app/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "/usr/src/app/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
+import "/usr/src/app/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "/usr/src/app/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "/usr/src/app/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "/usr/src/app/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "/usr/src/app/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "/usr/src/app/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "/usr/src/app/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "/usr/src/app/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "/usr/src/app/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import { default as default3 } from "/usr/src/app/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
+import { default as default4 } from "/usr/src/app/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
+import { default as default5 } from "/usr/src/app/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
+import { default as default6 } from "/usr/src/app/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default7 } from "/usr/src/app/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default8 } from "/usr/src/app/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default9 } from "/usr/src/app/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default10 } from "/usr/src/app/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
+import { default as default11 } from "/usr/src/app/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default12 } from "/usr/src/app/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default13 } from "/usr/src/app/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default14 } from "/usr/src/app/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 
 // node_modules/vitepress/dist/client/theme-default/support/utils.js
 import { withBase } from "vitepress";
